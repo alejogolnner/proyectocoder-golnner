5d6dbbc5f81c94042e885225dc4b2603740c4ab9
alejogolnner <alejogolnner@gmail.com>
2023-11-11 22:14:55 -0300

    storage y funcionalidad terminada

--- js/main.js
+++ js/main.js
@@ -1,103 +1,3 @@
-/*const TIPOS_RELLENO = ["DULCEDELECHE", "CREMA", "GANACHEDECHOCOLATE", "FRUTILLAS", "DURAZNOS"];
-const VALORES_RELLENO = [800, 950, 1000, 1300, 1900];
-const TIPOS_BIZCOCHUELOS = ["CHOCOLATE", "VAINILLA"];
-const VALORES_BISCOCHUELOS = [5000, 4500];
-const VALORES_TAMAniOS_TORTAS = [5000, 4000, 3000];
-
-let acumulado = 0;
-
-// Calculadora de Ganancia
-const calcularGanancia = (total) => {
-    let costoParcial = total - (total * 0.5);
-    let costoTotal = costoParcial + (total * 0.195);
-    let ganancia = total - costoTotal;
-    console.log("La ganancia es de $" + ganancia.toFixed(2));
-}
-
-// Solicitar al usuario que elija el tamanio de la torta
-let tamanioTorta = parseInt(prompt("Elija el tamanio de su torta entre las opciones en cm: 24, 20, 18"));
-
-// Validar que el tamanio elegido esté dentro de las opciones válidas
-while (![18, 20, 24].includes(tamanioTorta)) {
-    tamanioTorta = parseInt(prompt("El tamanio elegido no es una opción válida, por favor elija el tamanio en cm entre las opciones: 24, 20, 18"));
-}
-
-// Agregar el costo del tamanio de la torta seleccionado al costo acumulado
-acumulado += VALORES_TAMAniOS_TORTAS[tamanioTorta / 2 - 9];
-
-// Solicitar al usuario que elija el tipo de bizcochuelo
-let tipoBizcochuelo = prompt("Elija el tipo de bizcochuelo entre las opciones: chocolate o vainilla");
-
-// Validar que el tipo de bizcochuelo elegido esté dentro de las opciones válidas
-while (!TIPOS_BIZCOCHUELOS.includes(tipoBizcochuelo.trim().toUpperCase())) {
-    tipoBizcochuelo = prompt("El tipo de bizcochuelo elegido no está disponible, por favor elija entre una de las opciones: chocolate o vainilla");
-}
-
-// Agregar el costo del tipo de bizcochuelo seleccionado al costo acumulado
-acumulado += VALORES_BISCOCHUELOS[TIPOS_BIZCOCHUELOS.indexOf(tipoBizcochuelo.trim().toUpperCase())];
-
-// Solicitar al usuario la cantidad de rellenos deseada
-let cantRelleno = parseInt(prompt("Ingrese la cantidad de rellenos que desea para su torta (mínimo 1 y máximo 5)"));
-
-// Validar que la cantidad de rellenos esté dentro del rango correcto
-while (cantRelleno <= 0 || cantRelleno > 5 || isNaN(cantRelleno)) {
-    cantRelleno = parseInt(prompt("La cantidad de rellenos ingresada es inválida. Por favor, ingrese una cantidad de rellenos entre 1 y 5:"));
-}
-
-// Utilizar un bucle for para iterar sobre la cantidad de rellenos ingresados
-for (let i = 1; i <= cantRelleno; i++) {
-    // Solicitar al usuario que elija un tipo de relleno
-    let tipoRelleno = prompt("Elija el relleno entre las opciones: Dulce de leche, Crema, Ganache de chocolate, Frutillas, Duraznos");
-
-    // Limpiar y convertir a mayúsculas para una comparación insensible a mayúsculas/minúsculas
-    tipoRelleno = tipoRelleno.trim().toUpperCase().replace(/\s/g, "");
-
-    if (TIPOS_RELLENO.includes(tipoRelleno)) {
-        acumulado += VALORES_RELLENO[TIPOS_RELLENO.indexOf(tipoRelleno)];
-    } else {
-        i--; // Decrementamos i para que se repita la iteración
-        alert("El tipo de relleno ingresado no es válido. Por favor, elija uno de los tipos disponibles.");
-    }
-}
-
-// Mostrar el costo total acumulado de la torta
-alert("El costo total de su torta con " + cantRelleno + " rellenos es de: $" + acumulado);
-
-// Mostrar la ganancia total
-calcularGanancia(acumulado);
-
-
-//creo contructor de objetos para crear usuarios
-class Usuario{
-    constructor(name, surName, nickname, email, pasword){
-        this.nombre = name;
-        this.apellido = surName;
-        this.apodo = nickname
-        this.email = email;
-        this.contrasenia = pasword;
-    }
-    //retorno la funcion saludarUsuario para mostar el saludo en pantalla
-    saludarUsuario = () => {
-        return "Hola " + this.apodo + " bienvenido a la familia BENCHU!";
-    }
-}
-//inicializo variables para que el usuario cree su propio usuario
-let nombre = prompt("Ingrese su nombre")
-let apellido = prompt("Ingrese su apellido")
-let apodo = prompt("Ingrese su apodo")
-let email = prompt("Ingrese su email")
-let contrasenia = prompt("Cree su contrasenia")
-
-//con el constructor de objetos creo un usuario personalizado
-const primerUsuario = new Usuario (nombre, apellido, apodo, email, contrasenia)
-
-//saludo al usuario
-alert(primerUsuario.saludarUsuario())
-
-//muestro en consola al usuario
-console.log(primerUsuario);*/
-
-// Define un objeto que asocia los tipos de bizcochuelo con sus precios
 const tiposRellenoPrecios = {
     "Dulcedeleche": 800,
     "Crema": 950,
@@ -111,12 +11,65 @@ const tiposBizcochueloPrecios = {
     "Vainilla": 4500
 };
 
-// Valores de tamaños de tortas
-const tamañosTortaPrecios = {
+// Valores de tamanios de tortas
+const tamaniosTortaPrecios = {
     18 : 3000,
     20 : 4000,
     24 : 500
 }
+
+// Función para almacenar detalles de torta en localStorage
+const guardarDetallesTorta = (detalles) => {
+    localStorage.setItem('detallesTorta', JSON.stringify(detalles));
+}
+
+// Función para obtener detalles de torta almacenados en localStorage
+const obtenerDetallesTorta = () => {
+    const detallesGuardados = localStorage.getItem('detallesTorta');
+    return detallesGuardados ? JSON.parse(detallesGuardados) : null;
+}
+
+// Función para cargar detalles de torta en el formulario
+const cargarDetallesEnFormulario = () => {
+    const detallesGuardados = obtenerDetallesTorta();
+    if (detallesGuardados) {
+        document.getElementById("tamanioTorta").value = detallesGuardados.tamanioTorta;
+        document.getElementById("tipoBizcochuelo").value = detallesGuardados.tipoBizcochuelo;
+        document.getElementById("cantRelleno").value = detallesGuardados.cantRelleno;
+        
+        const tipoRellenoOptions = document.getElementById("tipoRelleno").options;
+        for (const relleno of detallesGuardados.tipoRellenos) {
+            const option = Array.from(tipoRellenoOptions).find(option => option.value === relleno);
+            if (option) {
+                option.selected = true;
+            }
+        }
+
+        recalcularTotal();
+    }
+}
+
+window.addEventListener('load', cargarDetallesEnFormulario);
+
+document.getElementById("calcular-button").addEventListener("click", () => {
+
+    // Guardar detalles de la torta en localStorage
+    const tamanioTorta = parseInt(document.getElementById("tamanioTorta").value);
+    const tipoBizcochuelo = document.getElementById("tipoBizcochuelo").value;
+    const cantRelleno = parseInt(document.getElementById("cantRelleno").value);
+    const tipoRellenoOptions = document.getElementById("tipoRelleno").options;
+    const tipoRellenos = Array.from(tipoRellenoOptions).filter(option => option.selected).map(option => option.value);
+
+    const detallesTorta = {
+        tamanioTorta,
+        tipoBizcochuelo,
+        cantRelleno,
+        tipoRellenos,
+    };
+
+    guardarDetallesTorta(detallesTorta);
+});
+
 
 // Función para calcular la ganancia
 const calcularGanancia = (total) => {
@@ -139,7 +92,7 @@ const recalcularTotal = () => {
     const tipoRellenoOptions = document.getElementById("tipoRelleno").options;
     const tipoRellenos = Array.from(tipoRellenoOptions).filter(option => option.selected).map(option => option.value);
 
-    let acumulado = tamañosTortaPrecios[tamanioTorta];
+    let acumulado = tamaniosTortaPrecios[tamanioTorta];
     acumulado += tiposBizcochueloPrecios[tipoBizcochuelo];
     for (const relleno of tipoRellenos) {
         if (tiposRellenoPrecios[relleno]) {
@@ -156,7 +109,6 @@ document.getElementById("tipoBizcochuelo").addEventListener("change", recalcular
 document.getElementById("cantRelleno").addEventListener("change", recalcularTotal);
 document.getElementById("tipoRelleno").addEventListener("change", recalcularTotal);
 
-// Calcular y mostrar la ganancia total cuando se hace clic en el botón "Calcular"
 document.getElementById("calcular-button").addEventListener("click", () => {
     const tamanioTorta = parseInt(document.getElementById("tamanioTorta").value);
     const tipoBizcochuelo = document.getElementById("tipoBizcochuelo").value;
@@ -165,7 +117,7 @@ document.getElementById("calcular-button").addEventListener("click", () => {
     const tipoRellenos = Array.from(tipoRellenoOptions).filter(option => option.selected).map(option => option.value);
 
     // Calcular el costo total acumulado de la torta
-    let acumulado = tamañosTortaPrecios[tamanioTorta];
+    let acumulado = tamaniosTortaPrecios[tamanioTorta];
     acumulado += tiposBizcochueloPrecios[tipoBizcochuelo];
     for (const relleno of tipoRellenos) {
         if (tiposRellenoPrecios[relleno]) {
